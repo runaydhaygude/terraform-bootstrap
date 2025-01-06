be6210d095c9d65975cd33be4555b770e0a1c2ca
runaydhaygude <runaydhaygude97@gmail.com>
2025-01-06 14:18:14 +0530

    multiple provider instance across different region

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# aws provider version
+# aws provider version #not needed if maintaining the .terraform.lock.hcl
 terraform {
   required_providers {
     aws = {
@@ -9,9 +9,30 @@ terraform {
 }
 
 
+
+#make sure the aws region is enabled for your aws account
 provider "aws" {
     region = "ap-south-1"
 }
+
+provider "aws" {
+    region = "us-east-2"
+    alias = "ohio"
+}
+
+
+
+
+resource "aws_vpc" "n_mumbai_vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "ohio_vpc" {
+    cidr_block = "10.1.0.0/16"
+    provider = aws.ohio
+}
+
+
 
 resource "aws_s3_bucket" "first_bucket" {
     bucket = "runay-tf-first-bucket"
